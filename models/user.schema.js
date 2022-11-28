223adeadc0ca6f66dd68ceb0f4772a98bece11c6
Padmanabh Belavadi <66545466+Padmanabh-B@users.noreply.github.com>
2022-11-28 01:23:12 +0000

    added user model part 1

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 import jwt from "jsonwebtoken";
+
 
 const userSchema = mongoose.Schema(
     {
@@ -8,6 +10,31 @@ const userSchema = mongoose.Schema(
             required:[true, "Name is required"],
             maxlength:[50, "Name must be under 50"],
             trim:true,
-        }
+        },
+        email:{
+            type:String,
+            required:[true, "Email is required"],
+            unique:true,
+        },
+        password:{
+            type:String,
+            required:[true, "password is required"],
+            minlength:[8, "password must be at least 8 characters"],
+            select:false,
+        },
+        role:{
+            type:String,
+            enum:Object.values(AuthRoles),
+            default:AuthRoles.USER,
+        },
+        forgotPasswordToken:String,
+        forgotPasswordExpiry:Date,
+
+
+    },
+    {
+        timestamps: true
     }
-)
+);
+
+export default mongoose.model("User", userSchema);
