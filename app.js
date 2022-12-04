2c3fa2c43a8cd5fc223dc742dde855df2b444f87
Padmanabh Belavadi <66545466+Padmanabh-B@users.noreply.github.com>
2022-12-04 06:13:09 +0000

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-import express from "express"
+import express from "express";
+import cookieParser from 'cookie-parser'
+import cors from "cors"
 
 const app = express();
+
+//Middleware
+
+app.use(express.json())
+app.use(express.urlencoded,{extended:true})
+app.use(cors())
+app.use(cookieParser())
+
+//Morgan Logger
+app.use(morgon('tiny'))
+
 
 
 
