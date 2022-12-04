7609afc311ad4b4c4d301f193c89e262f27d3797
Padmanabh Belavadi <66545466+Padmanabh-B@users.noreply.github.com>
2022-12-04 05:06:20 +0000

    product schema added

--- models/products.schema.js
+++ models/products.schema.js
@@ -1,12 +1,50 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 
 const productSchema = mongoose.Schema({
-    name:{
-        type:String,
+    name: {
+        type: String,
+        required: [true, "Please Provide a Product Name"],
+        trim: true,
+        maxLength: [120, "Product name should be a max of 120 charachters"]
+
+    },
+    price: {
+        type: Number,
+        required: [true, "Please Provide a Product Price"],
+        maxLength: [5, "Product price should be not be more than 5 digits"]
+    },
+    description: {
+        type: String,
+        //use some form of editor - personal assinment
+    },
+    photos: [
+        {
+            secure_url: {
+                type: String,
+                require: true,
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    collectionId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Collection",
         
+
     }
-})
+
+},
+    {
+        timestamps: true,
+    })
 
 
 
